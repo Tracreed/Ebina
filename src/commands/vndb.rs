181430e5cffdb54ad3f08fb1b7fa9aa241af9807
Tracreed <davidalasow@gmail.com>
2021-02-07 23:05:43 +0100

    Changed some vndb stuff

--- src/commands/vndb.rs
+++ src/commands/vndb.rs
@@ -9,82 +9,6 @@ use vndb::protocol::message;
 #[command]
 pub async fn vn(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let title = args.single_quoted::<String>().unwrap();
-
-    let get = message::request::Get {
-        kind: message::request::get::Type::vn(),
-        flags: message::request::get::Flags::new().basic().details(),
-        filters: message::request::get::Filters::new().filter(format!("title ~ \"{}\"", title.replace("\\", "").replace("\"", "\\\""))),
-        options: Some(message::request::get::Options {
-            page: Some(1),
-            results: None,
-            sort: None,
-            reverse: Some(true)
-        })
-    };
-
-    println!("{}", get);
-
-    let mut login = vndb::protocol::message::request::Login{
-        protocol: 1,
-        client: "EbinaReed",
-        clientver: 0.1,
-        creds: None,
-    };
-
-    client.send(&login.into()).await.expect("send");
-    client.send(&message::Request::DBstats).await.expect("stats");
-    client.send(&get.into()).await.expect("Visual novel");
-
-    /*match client.receive().await {
-        message::Response::Ok => println!("Got back data!"),
-        response => println!("Not good response"),
-    }*/
-    match client.receive().await.expect("To receive message").expect("To not fail receiving") {
-        message::Response::Ok => println!("Ok"),
-        response => panic!("Unexpected response={:?}", response),
-    }
-
-    /*match client.receive().await.expect("To receive message").expect("To not fail receiving") {
-        message::Response::DBstats(response) => {
-            println!("DBstats={:?}", response);
-        },
-        response => panic!("Unexpected response={:?}", response),
-    }*/
-
-    let info = match client.receive().await {
-        Ok(resp) => {
-            match resp {
-                Some(response) => response,
-                None => {
-                    return Ok(())
-                }
-            }
-        },
-        Err(why) => panic!("Something went wrong when getting info: {}", why)
-    };
-
-    let vn = match client.receive().await {
-        Ok(resp) => {
-            match resp {
-                Some(response) => response,
-                None => {
-                    return Ok(())
-                }
-            }
-        },
-        Err(why) => panic!("Something went wrong when getting info: {}", why)
-    };
-
-    println!("{:?}", info);
-    match vn {
-        vndb::protocol::Response::Ok => {}
-        vndb::protocol::Response::Results(vnb)  => {
-            let vnn = vnb.vn()?.items;
-            println!("{:?}", vnn[0].title.as_ref().unwrap());
-        },
-        vndb::protocol::Response::DBstats(_) => {}
-        vndb::protocol::Response::Error(_) => {}
-    };
     Ok(())
 
 }
@@ -127,7 +51,7 @@ async fn getvn(title: String) -> Result<Vec<vndb::protocol::message::response::r
             println!("{:?}", vnn[0].title.as_ref().unwrap());
             Ok(vnn)
         }
-        vndb::protocol::Response::Ok => {}
+        vndb::protocol::Response::Ok => {Ok(())}
         vndb::protocol::Response::DBstats(_) => {}
         vndb::protocol::Response::Error(_) => {}
     };
