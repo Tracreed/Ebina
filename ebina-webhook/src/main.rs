d63203038ffada608110609bc9791c5b7aa63d94
David Alasow <davidalasow@gmail.com>
2021-11-13 14:49:54 +0100

    Fixed Hmac and added commands.

--- ebina-webhook/src/main.rs
+++ ebina-webhook/src/main.rs
@@ -1,48 +1,70 @@
 use actix_web::http::{header, Method, StatusCode};
+use actix_web::web::Buf;
 use actix_web::{
-    error, post, guard, middleware, web, App, Error, HttpRequest, HttpResponse,
-    HttpServer, Result,
+	error, post, guard, middleware, web, App, Error, HttpRequest, HttpResponse,
+	HttpServer, Result,
 };
 
-use futures::StreamExt;
-
 use sha2::Sha256;
-
 use hmac::{Hmac, Mac, NewMac};
+use hex::encode;
+
+use std::process::Command;
 
 extern crate dotenv;
 
 type HmacSha256 = Hmac<Sha256>;
 
 #[post("/rb")]
-async fn reboot(mut payload: web::Payload, request: HttpRequest) -> Result<HttpResponse> {
-    let secret = std::env::var("GITEA_SECRET").expect("GITEA_SECRET needs to be set");
-    let mut hmac = HmacSha256::new_from_slice(secret.as_bytes())
-        .expect("Something went wrong with creating the HMAC");
-    while let Some(chunk) = payload.next().await {
-        let chunk = chunk?;
-        hmac.update(&chunk.to_vec());
-    }
+async fn reboot(body: web::Bytes, request: HttpRequest) -> Result<HttpResponse> {
 
-    //let result = hmac.finalize();
+	if !request.headers().get("Content-Type").unwrap().eq("application/json") {
+		return Ok(HttpResponse::UnsupportedMediaType().body("415 Unsupported Media Type"))
+	}
 
-    let gitea_hmac = request.headers().get("X-Gitea-Signature").unwrap().as_bytes();
+	let secret = std::env::var("GITEA_SECRET").expect("GITEA_SECRET needs to be set");
 
-    hmac.verify(gitea_hmac).unwrap();
+	let mut hmac = HmacSha256::new_from_slice(secret.as_bytes())
+		.expect("Something went wrong with creating the HMAC");
 
-    println!("Test");
+	hmac.update(body.bytes());
+	let hashed = encode(hmac.finalize().into_bytes());
 
-Ok(HttpResponse::Ok().body(""))
+	let gitea_hmac = request.headers().get("X-Gitea-Signature").unwrap();
+
+	if gitea_hmac.eq(&hashed) {
+		Command::new("git")
+			.arg("pull")
+			.output()
+			.expect("failed to git pull");
+		Command::new("cargo")
+			.arg("build")
+			.arg("--release")
+			.output()
+			.expect("failed to cargo build");
+		Command::new("cp")
+			.arg("./target/release/ebina-bot")
+			.arg("./bot")
+			.output()
+			.expect("failed to copy built bot");
+		Command::new("systemctl")
+			.arg("restart")
+			.arg("ebina.service")
+			.output()
+			.expect("failed to restart service");
+	}
+
+	Ok(HttpResponse::Ok().body(""))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().expect("Failed to load .env file");
-    HttpServer::new(|| {
-        App::new()
-        .service(reboot)
-    })
-    .bind("0.0.0.0:25566")?
-    .run()
-    .await
+	dotenv::dotenv().expect("Failed to load .env file");
+	HttpServer::new(|| {
+		App::new()
+		.service(reboot)
+	})
+	.bind("127.0.0.1:25566")?
+	.run()
+	.await
 }
